fef4d8924d4113bd09e79e5fe7d6c9ae692a02b7
Thorben Amrhein <thorben.amrhein@haw-hamburg.de>
2021-05-05 10:17:49 +0200

    adding database initialisation

---
+++ initdbvalues.sql
@@ -0,0 +1,11 @@
+INSERT INTO users 
+    (username, password, email)
+VALUES
+    ("plantfriend", "abc123AB", "pf@gmail.com")
+;
+
+INSERT INTO posts 
+    (userid, title, txt)
+VALUES
+    (1, "First posts", "This is my first post")
+;
