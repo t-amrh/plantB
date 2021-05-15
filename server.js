e0cede4dc5b19231a152322e1a4c459e60145910
Thorben Amrhein <thorben.amrhein@haw-hamburg.de>
2021-05-15 12:33:03 +0200

    adding answers view and delete_answer

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(session({
     secret: 'example',
     resave: true,
     saveUninitialized: true,
+    rolling: true,
     cookie: {
         maxAge: 2 * 60*60*60*1000,
         sameSite: 'none',
@@ -54,6 +55,7 @@ app.get('/home', (req, res) => {
     }
 });
 
+
 app.get('/login', (req, res) => {
     if (req.session.user){
         res.redirect('/welcome');
@@ -67,6 +69,7 @@ app.get('/register', (req, res) => {
     res.render('register', {});
 });
 
+
 app.get('/profile', (req, res) => {
     if (req.session.user)
         res.render('profile', {task: 'overview', username: req.session.user.name})
@@ -79,6 +82,7 @@ app.get('/change_pw', (req, res) => {
 app.get('/delete_user', (req, res) => {
     res.render('profile', {task: 'delete_user', username: req.session.user.name})
 });
+
 
 app.get('/welcome', (req, res) => {
     if (req.session.user){
@@ -94,12 +98,14 @@ app.get('/logout', (req,res) => {
     res.redirect('/home');
 });
 
+
 app.get('/create_post', (req,res) => {
     if (req.session.user)
         res.render('create_post', {});
     else 
         res.redirect('/home');
 });
+
 
 app.get('/create_question', (req,res) => {
     if (req.session.user)
@@ -113,23 +119,116 @@ app.get('/questions', (req, res) => {
         if (err){console.error(err.message)};
     });
 
-    sql = `SELECT users.username AS name, title, txt, tstamp, questions.img AS img FROM users, questions WHERE users.id = questions.userid`;
-    db.all(sql, (err, rows) => {
+    let sql = `SELECT 
+                users.username AS name, title, txt, tstamp, questions.img AS img, questions.id AS id
+                FROM 
+                    users, questions 
+                WHERE 
+                    users.id = questions.userid`;
+    db.all(sql, (err, questions) => {
         if (err){console.error(err.message)};
 
-        if (rows && rows.length > 0){
-            res.render('questions', {questions: rows})
+        if (questions && questions.length > 0){
+            res.render('questions', {questions})
         }
         else{
             res.redirect('/create_question')
         }
     })
+    db.close();
+});
+
+
+//question detail view
+
+app.get('/question/:id', (req, res) => {
+
+    let db = new sqlite3.Database('plant.db', (err) => {
+        if (err){console.error(err.message)};
+    });
+
+    let sql = `SELECT 
+                users.username AS name, title, txt, tstamp, questions.img AS img, questions.id AS id
+            FROM 
+                users, questions 
+            WHERE 
+                users.id = questions.userid
+            AND
+                questions.id=?`;
+
+    db.get(sql, [req.params.id], (err, question) => {
+        if (err){console.error(err.message)};
+
+        if (question){
+
+            let sql = `SELECT 
+                            tstamp, txt, votes, users.username AS name, users.id AS userid, answers.id as id
+                        FROM 
+                            answers, users 
+                        WHERE 
+                            questionid=?
+                        AND 
+                            users.id=answers.userid`;
+            db.all(sql, [question.id], (err, answers) => {
+                if (err){console.error(err.message)};
+
+                if (answers && answers.length > 0){
+                    res.render('question', {question, answers, userid: req.session.user.id})
+                }
+                else{
+                    res.render('question', {question});
+                }
+            });  
+        }
+        else{
+            res.redirect('/questions');
+        }
+    })
+   db.close();
+});
+
+
+//create_aswer POST handling 
+
+app.post('/create_answer', (req, res) => {
+
+    let db = new sqlite3.Database('plant.db', (err) => {
+        if (err){console.error(err.message)};
+    });
+
+    let sql = `INSERT INTO 
+                        answers (tstamp, questionid, userid, txt, votes)
+                    VALUES
+                        ( datetime('now', 'localtime'),?, ?, ?, 0)`;
+    
+    db.run(sql, [req.body.questionid, req.session.user.id, req.body.txt], (err) => {
+        if (err) {console.error(err.message) };
+        res.redirect('/question/'+req.body.questionid)
+    })
+    db.close();
+
 })
 
 
+app.post('/delete_answer/:id', (req, res) => {
+    let db = new sqlite3.Database('plant.db', (err) => {
+        if (err){console.error(err.message)};
+    });
+
+    let sql = `DELETE FROM
+                    answers
+                WHERE
+                    id=?`
+    db.run(sql, [req.params.id], (err) => {
+        if (err) {console.error(err.message)}
+        res.redirect('back');
+    })
+    db.close();
+})
+
+
+
 // create_question POST handling
-
-
 app.post('/create_question', (req, res) => {
 
     let path = 'images/users/' ; //+ req.session.user.id + "/"
@@ -147,7 +246,7 @@ app.post('/create_question', (req, res) => {
     });
 
     let sql = `INSERT INTO questions (tstamp, userid, title, txt, img) VALUES (datetime('now', 'localtime'), ?, ?, ?, ?)`;
-    db.run(sql, [req.session.user.id, req.body.title, req.body.txt, path + filename], (err) => {
+    db.run(sql, [req.session.user.id, req.body.title, req.body.txt, path+filename], (err) => {
         if (err) console.error(err.message); 
     });
 
@@ -156,6 +255,7 @@ app.post('/create_question', (req, res) => {
     res.redirect("/questions");
 
 })
+
 
 // login POST handling
 app.post('/login', 
@@ -170,7 +270,6 @@ app.post('/login',
         let sql = `SELECT password, id FROM users WHERE username=?`;
         db.get(sql, [req.body.username], (err,row) => {
             if (err) {console.error(err.message)};
-
 
             if (row && (passwordHash.verify(req.body.password, row.password) ) ){
                 req.body.id = row.id;
@@ -265,7 +364,6 @@ app.post('/register',
                 req.session.user = {
                     name: row.username,
                     id: row.id,
-                    loggedin: true,
                 };
                 res.redirect('/welcome');
             
